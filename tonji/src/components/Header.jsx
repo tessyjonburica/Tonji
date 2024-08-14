2dbc382422974d8fba04664dbabb95d0022b7280
Teslim <yusuftessy2005@gmail.com>
2024-08-14 18:19:47 +0100

    Updated index.html, App.css, Header.jsx,

--- tonji/src/components/Header.jsx
+++ tonji/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="flex justify-center space-x-4 py-4 text-white font-bold border-2 border-red-600"  style={{ backgroundColor: '#1d1c18' }}>
+    <header className="flex justify-center space-x-4 py-4 text-white font-Montserrat font-semibold border-2 border-red-600"  style={{ backgroundColor: '#1d1c18' }}>
       <a href="#">DexTools</a>
       <a href="#">Telegram</a>
       <span>X</span>
--- tonji/tailwind.config.js
+++ tonji/tailwind.config.js
@@ -1,11 +1,26 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+        mooLahLah: ['Moo Lah Lah', 'cursive'],
+      },
+      fontWeight: {
+        thin: 100,
+        extralight: 200,
+        light: 300,
+        normal: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+        extrabold: 800,
+        black: 900,
+      },
+    },
   },
   plugins: [],
 }
